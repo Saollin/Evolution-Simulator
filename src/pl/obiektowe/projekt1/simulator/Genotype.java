432368b198931e20ed442f8d7c999cef3c199e91
Saollin <gregory.janosz@gmail.com>
2019-12-09 01:07:27 +0100

    Add class Genotype

---
+++ src/pl/obiektowe/projekt1/simulator/Genotype.java
@@ -0,0 +1,9 @@
+package pl.obiektowe.projekt1.simulator;
+
+public class Genotype {
+    private static int numberOfGenes = 32;
+
+    private int[] genes = new int[numberOfGenes];
+    private int indexEndingFirstGenesGroup;
+    private int indexEndingSecondGenesGroup;
+}
