978c691ae479a352b500cd6ddcae831967ac7b62
Saollin <gregory.janosz@gmail.com>
2019-12-12 13:10:31 +0100

    Add createNewGenotypeWithSecondParent and field genotypeOfAnimal to Animal.
    It makes way to reproduction of two animal;

--- src/main/java/pl/obiektowe/projekt1/simulator/Animal.java
+++ src/main/java/pl/obiektowe/projekt1/simulator/Animal.java
@@ -3,9 +3,19 @@ package pl.obiektowe.projekt1.simulator;
 public class Animal {
     private Position position;
     private int energy;
+    private Genotype genotypeOfAnimal;
 
     public Animal(Position startPosition, int startEnergy){
         this.position = startPosition;
         this.energy = startEnergy;
+        this.genotypeOfAnimal = new Genotype();
     }
+
+    public Animal(Position startPosition, int startEnergy, int [] genotype){
+        this.position = startPosition;
+        this.energy = startEnergy;
+        this.genotypeOfAnimal = new Genotype(genotype);
+    }
+
+
 }
--- src/main/java/pl/obiektowe/projekt1/simulator/Genotype.java
+++ src/main/java/pl/obiektowe/projekt1/simulator/Genotype.java
@@ -1,13 +1,20 @@
 package pl.obiektowe.projekt1.simulator;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Genotype {
     private static int numberOfGenes = 32;
 
     private int[] genes = new int[numberOfGenes];
-    private int indexEndingFirstGenesGroup;
-    private int indexEndingSecondGenesGroup;
+
+    Genotype(){
+        this.genes = generateRandomGenes();
+    }
+
+    Genotype(int [] genes){
+        this.genes = genes;
+    }
 
     public static int[] generateRandomGenes(){
         int [] numberOfGenesOfType = new int[8];
@@ -30,5 +37,35 @@ public class Genotype {
         return result;
     }
 
+    public int [] createNewGenotypeWithSecondParent(Genotype parent2){
+        Random generator = new Random();
+        int firstIndex = generator.nextInt(numberOfGenes);
+        int secondIndex = generator.nextInt(numberOfGenes);
+        while(secondIndex == firstIndex)
+            secondIndex = generator.nextInt(numberOfGenes);
+        int [] genesOfSecondParent = parent2.getGenes();
+        int [] firstPartOfGenes;
+        int [] secondPartOfGenes;
+        if (firstIndex < secondIndex) {
+            firstPartOfGenes = Arrays.copyOfRange(this.genes, 0, firstIndex);
+            secondPartOfGenes = Arrays.copyOfRange(genesOfSecondParent, firstIndex + 1, numberOfGenes - 1);
+        } else {
+            firstPartOfGenes = Arrays.copyOfRange(genesOfSecondParent, 0, secondIndex);
+            secondPartOfGenes = Arrays.copyOfRange(this.genes, secondIndex + 1, numberOfGenes - 1);
+        }
+        int [] genesOfChildren = new int[numberOfGenes];
+        for(int i = 0; i < numberOfGenes; i++){
+            if(i<firstPartOfGenes.length){
+                genesOfChildren[i] = firstPartOfGenes[i];
+            }
+            else{
+                genesOfChildren[i] = secondPartOfGenes[i - firstPartOfGenes.length];
+            }
+        }
+        return genesOfChildren;
+    }
 
+    public int[] getGenes() {
+        return genes;
+    }
 }
