33c2b4630ea69a04c2b799b6283ae76b1e588779
Saollin <gregory.janosz@gmail.com>
2019-12-15 17:47:01 +0100

    Add interface IWorldMap

---
+++ src/main/java/pl/obiektowe/projekt1/simulator/IWorldMap.java
@@ -0,0 +1,42 @@
+package pl.obiektowe.projekt1.simulator;
+
+import pl.obiektowe.projekt1.simulator.IMapElement;
+import pl.obiektowe.projekt1.simulator.Position;
+
+public interface IWorldMap {
+
+    /**
+     * Indicate if any object can move to the given position.
+     *
+     * @param position The position checked for the movement possibility.
+     * @return True if the object can move to that position.
+     */
+    boolean canMoveTo(Position position);
+
+    /**
+     * Place a animal on the map.
+     *
+     * @param element The element to place on the map.
+     * @return True if the animal was placed. The animal cannot be placed if the map is already occupied.
+     */
+    boolean place(IMapElement element);
+
+
+    /**
+     * Return true if given position on the map is occupied. Should not be
+     * confused with canMove since there might be empty positions where the animal
+     * cannot move.
+     *
+     * @param position Position to check.
+     * @return True if the position is occupied.
+     */
+    boolean isOccupied(Position position);
+
+    /**
+     * Return an object at a given position.
+     *
+     * @param position The position of the object.
+     * @return Object or null if the position is not occupied.
+     */
+    Object objectAt(Position position);
+}
