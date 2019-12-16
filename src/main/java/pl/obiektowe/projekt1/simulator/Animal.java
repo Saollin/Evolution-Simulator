c79b20b4c05e9a4e299c2f8037062a936c42e87b
Saollin <gregory.janosz@gmail.com>
2019-12-16 01:59:33 +0100

    Add move() and copulate() to Animal

--- src/main/java/pl/obiektowe/projekt1/simulator/Animal.java
+++ src/main/java/pl/obiektowe/projekt1/simulator/Animal.java
@@ -40,7 +40,7 @@ public class Animal implements IMapElement{
         observers.remove(observer);
     }
 
-    public void notifyObserver(Vector2d oldPosition, Vector2d newPosition){
+    public void notifyObservers(Vector2d oldPosition, Vector2d newPosition){
         for(IPositionChangeObserver observer:observers){
             observer.positionChanged(oldPosition,newPosition);
         }
@@ -52,8 +52,48 @@ public class Animal implements IMapElement{
         this.directionOfAnimal = newDirection;
     }
 
+    public void move(MoveDirection moveDirection){
+        Vector2d unitVector, reverseUnitVector, newPosition, oldPosition;
+        switch (moveDirection){
+            case FORWARD:
+                unitVector = this.directionOfAnimal.toUnitVector();
+                oldPosition = this.getPosition();
+                newPosition = position.add(unitVector);
+                if (map.canMoveTo(newPosition)) {
+                    this.position = newPosition;
+                    this.notifyObservers(oldPosition,newPosition);
+                }
+                break;
+            case BACKWARD:
+                unitVector = this.directionOfAnimal.toUnitVector();
+                oldPosition = this.getPosition();
+                newPosition = position.subtract(unitVector);
+                if (map.canMoveTo(newPosition)) {
+                    this.position = newPosition;
+                    this.notifyObservers(oldPosition,newPosition);
+                }
+                break;
+            default:
+                return;
+        }
+    }
+
+    public void copulate(Animal secondParent){
+        int childEnergy = (int)(0.25 * this.energy) + (int)(0.25 * secondParent.energy);
+        this.changeEnergy((int)-(0.25 * this.energy));
+        secondParent.changeEnergy((int)-(0.25 * secondParent.energy));
+        Vector2d positionOfChild;
+        Genotype genotypeOfChild = this.genotypeOfAnimal.createNewGenotypeWithSecondParent(secondParent.getGenotypeOfAnimal());
+        Animal child = new Animal(map,positionOfChild,childEnergy, genotypeOfChild);
+    }
+
     @Override
     public Vector2d getPosition() {
         return this.position;
     }
+
+    public Genotype getGenotypeOfAnimal() {
+        return genotypeOfAnimal;
+    }
+
 }
