02e4cd9c4b88a8d33f544d4a028cb2b0b7547966
Saollin <gregory.janosz@gmail.com>
2019-12-16 01:40:23 +0100

    Add test to MapDirection

---
+++ src/test/java/pl/obiektowe/projekt1/simulator/MapDirectionTest.java
@@ -0,0 +1,46 @@
+package pl.obiektowe.projekt1.simulator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapDirectionTest {
+
+    @Test
+    void classShouldReturnRightUnitVectors(){
+        Vector2d north = new Vector2d(0,1);
+        Vector2d northEast = new Vector2d(1,1);
+        Vector2d east = new Vector2d(1,0);
+        Vector2d southEast = new Vector2d(1,-1);
+        Vector2d south = new Vector2d(0,-1);
+        Vector2d southWest = new Vector2d(-1,-1);
+        Vector2d west = new Vector2d(-1,0);
+        Vector2d northWest = new Vector2d(-1, 1);
+        assertAll(()->{
+            assertEquals(north, MapDirection.NORTH.toUnitVector());
+            assertEquals(northEast, MapDirection.NORTH_EAST.toUnitVector());
+            assertEquals(east, MapDirection.EAST.toUnitVector());
+            assertEquals(southEast, MapDirection.SOUTH_EAST.toUnitVector());
+            assertEquals(south, MapDirection.SOUTH.toUnitVector());
+            assertEquals(southWest, MapDirection.SOUTH_WEST.toUnitVector());
+            assertEquals(west, MapDirection.WEST.toUnitVector());
+            assertEquals(northWest, MapDirection.NORTH_WEST.toUnitVector());
+        });
+    }
+
+    @Test
+    void classShouldReturnRightMapDirectionToTheRightNumber(){
+        assertAll(()->{
+            assertEquals(MapDirection.NORTH, MapDirection.valueOfDirectionNumber(0));
+            assertEquals(MapDirection.NORTH_EAST, MapDirection.valueOfDirectionNumber(1));
+            assertEquals(MapDirection.EAST, MapDirection.valueOfDirectionNumber(2));
+            assertEquals(MapDirection.SOUTH_EAST, MapDirection.valueOfDirectionNumber(3));
+            assertEquals(MapDirection.SOUTH, MapDirection.valueOfDirectionNumber(4));
+            assertEquals(MapDirection.SOUTH_WEST, MapDirection.valueOfDirectionNumber(5));
+            assertEquals(MapDirection.WEST, MapDirection.valueOfDirectionNumber(6));
+            assertEquals(MapDirection.NORTH_WEST, MapDirection.valueOfDirectionNumber(7));
+        });
+    }
+}
