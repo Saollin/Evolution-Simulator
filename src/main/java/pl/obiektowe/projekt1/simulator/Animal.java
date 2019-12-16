dde0b6d27855fc89cd9decc0eea5d4a384e28c47
Saollin <gregory.janosz@gmail.com>
2019-12-16 01:20:59 +0100

    Change constructors of Animal and add methods to handle Observers.

--- src/main/java/pl/obiektowe/projekt1/simulator/Animal.java
+++ src/main/java/pl/obiektowe/projekt1/simulator/Animal.java
@@ -1,22 +1,50 @@
 package pl.obiektowe.projekt1.simulator;
 
+import java.util.ArrayList;
+
 public class Animal implements IMapElement{
+
+    private MapDirection directionOfAnimal;
+    private IWorldMap map;
     private Vector2d position;
     private int energy;
+    private ArrayList<IPositionChangeObserver> observers = new ArrayList<>();
     private Genotype genotypeOfAnimal;
 
-    public Animal(Vector2d startPosition, int startEnergy){
+    public Animal(IWorldMap map, Vector2d startPosition, int startEnergy){
+        this.map = map;
         this.position = startPosition;
         this.energy = startEnergy;
+        this.directionOfAnimal = MapDirection.NORTH;
         this.genotypeOfAnimal = new Genotype();
     }
 
-    public Animal(Vector2d startPosition, int startEnergy, int [] genotype){
-        this.position = startPosition;
-        this.energy = startEnergy;
-        this.genotypeOfAnimal = new Genotype(genotype);
+    public Animal(IWorldMap map, Vector2d startPosition, int startEnergy, Genotype genotype){
+        this(map,startPosition,startEnergy);
+        this.genotypeOfAnimal = genotype;
     }
 
+    public boolean isDead(){
+        return this.energy <= 0;
+    }
+
+    public void changeEnergy(int value){
+        this.energy += value;
+    }
+
+    public void addObservers(IPositionChangeObserver observer){
+        observers.add(observer);
+    }
+
+    public void removeObservers(IPositionChangeObserver observer){
+        observers.remove(observer);
+    }
+
+    public void notifyObserver(Vector2d oldPosition, Vector2d newPosition){
+        for(IPositionChangeObserver observer:observers){
+            observer.positionChanged(oldPosition,newPosition);
+        }
+    }
 
     @Override
     public Vector2d getPosition() {
