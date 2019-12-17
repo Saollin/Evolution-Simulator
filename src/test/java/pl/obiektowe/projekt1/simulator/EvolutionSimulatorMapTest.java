8a7be5ece67116e9460c6d05b16817643d7bdd1b
Saollin <gregory.janosz@gmail.com>
2019-12-18 00:33:59 +0100

    Add tests for creating new EvolutionSimulatorMap and place() method

--- src/test/java/pl/obiektowe/projekt1/simulator/EvolutionSimulatorMapTest.java
+++ src/test/java/pl/obiektowe/projekt1/simulator/EvolutionSimulatorMapTest.java
@@ -11,6 +11,13 @@ class EvolutionSimulatorMapTest {
     private EvolutionSimulatorMap map = new EvolutionSimulatorMap(100,100,0.25,20,1, 4);
 
     @Test
+    void shouldCreatingNewMapLastNoLongerThan5Ms(){
+        assertTimeout(Duration.ofMillis(5), () -> {
+            EvolutionSimulatorMap newMap = new EvolutionSimulatorMap(100,100,0.5, 20,1,10);
+        });
+    }
+
+    @Test
     void positionSmallerXThanLowerLeftShouldBeAtTheSameDistanceFromRightBound() {
         //given
         Vector2d wrongPosition = new Vector2d(-1,10);
@@ -62,12 +69,91 @@ class EvolutionSimulatorMapTest {
     }
 
     @Test
-    void shouldSpawnGrassLastNoLongerThan10Ms(){
+    void placedAnimalIsInsideLists(){
+        //given
+        Vector2d position = new Vector2d(2,2);
+        Animal animal = new Animal(map, position, 40);
+
+        //when
+        map.place(animal);
+
+        //then
+        assertAll(()->{
+            assertEquals(animal, map.getAnimals().get(position).getFirst());
+            assertTrue(map.getAnimalList().contains(animal));
+        });
+    }
+
+    @Test
+    void placedPlantIsInsideLists(){
+        //given
+        Vector2d position = new Vector2d(2,2);
+        Plant plant = new Plant(position);
+
+        //when
+        map.place(plant);
+
+        //then
+        assertAll(()->{
+            assertEquals(plant, map.getPlants().get(position));
+        });
+    }
+
+    @Test
+    void plantIsNotPlacedInThePositionWhichIsOccupied(){
+        //given
+        Vector2d position = new Vector2d(2,2);
+        Plant plant1 = new Plant(position);
+        Plant plant2 = new Plant(position);
+
+        //when
+        map.place(plant1);
+
+        //then
+        assertAll(()->{
+            assertFalse(map.place(plant2));
+            assertNotEquals(plant2, map.getPlants().get(position));
+        });
+    }
+
+    @Test
+    void shouldSpawnGrassLastNoLongerThan5MsAtFirstSpawning(){
         //given
         //when
         //then
         assertTimeout(Duration.ofMillis(5), () -> {
             map.spawnGrassInSteppeAndJungle();
         });
+    }
+
+    /*
+    After 500 spawning should be all positions occupied.
+     */
+    @Test
+    void shouldSpawnGrassLastNoLongerThan5MsAfter500Spawning(){
+        //given
+        //when
+        for(int i = 0; i < 500; i++){
+            map.spawnGrassInSteppeAndJungle();
+        }
+        //then
+        assertTimeout(Duration.ofMillis(5), () -> {
+            map.spawnGrassInSteppeAndJungle();
+        });
+    }
+
+    @Test
+    void shouldCanPlantBePlacedReturnTrueForEmptyPlace(){
+        EvolutionSimulatorMap newMap = new EvolutionSimulatorMap(50,50,0.5,30,1,10);
+        assertTrue(newMap.canPlantBePlaced(new Vector2d(3,3)));
+    }
+
+    @Test
+    void shouldCanPlantBePlacedReturnFalseForOccupiedPlace(){
+        EvolutionSimulatorMap newMap = new EvolutionSimulatorMap(50,50,0.5,30,1,10);
+        Vector2d position = new Vector2d(2,5);
+        Animal animal = new Animal(newMap,position,10);
+        newMap.place(animal);
+        assertFalse(newMap.canPlantBePlaced(position));
     }
 }
